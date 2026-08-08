48d1314c26ef5584431ad0440f1dbf3379bf4323
Arshad Ali <arshadalii8603@gmail.com>
2026-08-08 19:54:45 +0500

    JS - Journey Lesson # 2

--- script2.js
+++ script2.js
@@ -1,1 +1,35 @@
-script2.js
+// lesson # 2
+
+function greet(name) {
+    return `Hello, ${name}! Welcome to JS`
+}
+
+function calculateGrade(marks) {
+    if (marks>=90) {
+        return "A+"
+    }
+    else if (marks >= 80) {
+        return "A"
+    }
+    else if (marks>= 70){
+        return "B"
+    }
+    else if (marks>= 60){
+        return "C"
+    }
+    else if (marks >= 50) {
+        return "Pass"
+    }   
+    else {
+        return "Fail"
+    }
+    
+}
+
+const isPass = marks => marks >= 50
+
+
+
+console.log(greet("Arshad"))
+console.log(calculateGrade(9))
+console.log(isPass(6))
