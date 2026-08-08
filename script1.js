2f060eb6898f9522179c74bbe784f4b87d4d657f
Arshad Ali <arshadalii8603@gmail.com>
2026-08-08 19:32:39 +0500

    JS - Journey Lesson # 1

---
+++ script1.js
@@ -0,0 +1,9 @@
+// Lesson # 1
+
+const name = "Arshad Ali"
+let semester = 5
+const IsCSStudent = true
+
+console.log("Name: ", name)
+console.log("Semester: ", semester)
+console.log("CS Student: ", IsCSStudent)
